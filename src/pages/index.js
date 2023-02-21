6c90899e3ec2453b2b58b90ede0af1b771529643
“xzkxzk123” <675332938@qq.com>
2023-02-21 17:17:48 +0800

    main

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,43 +6,39 @@ import IndexIntroduce from '../components/index-introduce.js';
 import IndexActivity from '../components/index-activity.js';
 import IndexContribute from '../components/index-contribute.js';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import '../css/index.css';
-// import styles from './index.module.css';
+// import '../css/index.css';
+import styles from './index.module.css';
 // import HomepageFeatures from '../components/HomepageFeatures';
 import Translate, {translate} from '@docusaurus/Translate';
+import Elephant from '../../svg/img-elephant-balloon.svg';
 
 function HomepageHeader() {
   return (
-    <div class="Outer-layer">
-    <div class="homepage-header" id="id-homepage-header">
-        <div class="header-font">
-            <p class="header-title">
-              <Translate>
-                Open Source PostgreSQL with Oracle
-                compatibility Features
-              </Translate>
-            </p>
-            {/* <p class="header-content">基于PostgreSQL兼容Oracle的开源数据库</p> */}
-        <div id="Link" class="Link">
-            <Link 
-              className="button button--secondary button--lg" 
-              id="header-link-getstart"
-              to="https://github.com/IvorySQL/IvorySQL">
-                <div class="button-font-getstart" id="id-button-font-getstart"><Translate>Get Started</Translate></div>
-            </Link>
-            <Link 
-              className="button button--secondary button--lg" 
-              id="header-link-viewmore"
-              to="https://github.com/IvorySQL/IvorySQL/blob/master/README.md">
-                <div class="button-font-viewmore" id="id-button-font-viewmore"><Translate>View More</Translate></div>
-            </Link>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <div class="row">
+
+          <div className={styles.heroCenterImage}>
+          <h1 className="hero__title"><Translate>IvorySQL</Translate></h1>
+        <p className="hero__subtitle"><Translate>Open Source Oracle compatible PostgreSQL</Translate></p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="https://github.com/IvorySQL/IvorySQL/blob/master/README.md">
+            <Translate>Learn More</Translate>
+          </Link>
+        </div>
+
+          </div>
+          <div className={styles.heroRightImage}>
+            <Elephant></Elephant>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
+    </header>
   );
 }
-
+                                          
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
